7e7ce084ae1aa8e788f9de7364e4ba2dcffe68ca
ViPhong <viphongly2804@gmail.com>
2024-04-09 10:38:35 +0700

    express-ws connected to Wemos

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const expressWs = require("express-ws");
 const logger = require("morgan");
 const createError = require("http-errors");
 require("dotenv").config();
 
 const { verifyAccessToken } = require("./helpers/jwt_helper");
-const { getUser, authUserDevice, authDeviceSensor } = require("./helpers/user_helper");
+const {
+  getUser,
+  authUserDevice,
+  authDeviceSensor,
+  authDeviceControlUnit,
+} = require("./helpers/user_helper");
 const TestRoute = require("./routes/Test.route");
 const AuthRoute = require("./routes/Auth.route");
 const DeviceRoute = require("./routes/Device.route");
@@ -12,9 +18,12 @@ const UserRoute = require("./routes/User.route");
 const SensorRoute = require("./routes/Sensor.route");
 const ControlUnitRoute = require("./routes/ControlUnit.route");
 const SensorReadingRoute = require("./routes/SensorReading.route");
+const CommandRoute = require("./routes/Command.route");
+const { transform } = require("dottie");
 
 const app = express();
-
+const appws = expressWs(app);
+const clients = appws.getWss().clients;
 // Middlewares
 app.use(logger("dev"));
 app.use(express.json());
@@ -44,6 +53,35 @@ app.use(
   authDeviceSensor,
   SensorReadingRoute
 );
+
+app.use(
+  "/user/device/:deviceId/control-unit/:controlUnitId/",
+  verifyAccessToken,
+  authUserDevice,
+  authDeviceControlUnit,
+  CommandRoute
+);
+app.ws("/", (ws, req) => {
+  console.log("Connected");
+  ws.send("Connected");
+
+  ws.on("close", (msg) => {
+    console.log("disconnected");
+  });
+
+  ws.on("message", (msg) => {
+    transformer = {
+      2: "0",
+      8: "1",
+      20: "2",
+    };
+    command = JSON.parse(msg);
+    clients.forEach((client) => {
+      console.log("client message:", command);
+      client.send(transformer[command.controlUnitId]);
+    });
+  });
+});
 
 //Error handler
 app.use((req, res, next) => {
